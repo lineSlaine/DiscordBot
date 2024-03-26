feda95c8e2ef7b70204f1c956c182df720dde735
musfobbie@mail.ru <musfobbie@mail.ru>
2024-03-26 10:28:15 +0300

    add command myguns

--- DiscordBot/Commands/TestCommand.cs
+++ DiscordBot/Commands/TestCommand.cs
@@ -1,5 +1,8 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.CommandsNext.Converters;
+using DSharpPlus.Entities;
+using System.Drawing;
 
 internal class TestCommand : BaseCommandModule
 {
@@ -8,5 +11,45 @@ internal class TestCommand : BaseCommandModule
     {
         
         await commandContext.Channel.SendMessageAsync($"Hello, {commandContext.User.Username}");
+    }
+
+    [Command ("myguns")]
+    public async Task MyGuns(CommandContext commandContext)
+    {
+        var firstGunEmber = new DiscordEmbedBuilder
+        {
+            Title = "First Gun",
+            Description = "Cost",
+            Color = DiscordColor.CornflowerBlue,
+        };
+        await commandContext.Channel.SendMessageAsync(embed: firstGunEmber);
+        var secondGunEmber = new DiscordEmbedBuilder
+        {
+            Title = "Second Gun",
+            Description = "Cost",
+            Color = DiscordColor.Aquamarine,
+        };
+        await commandContext.Channel.SendMessageAsync(embed: secondGunEmber);
+        var thirdGunEmber = new DiscordEmbedBuilder
+        {
+            Title = "Third Gun",
+            Description = "Cost",
+            Color = DiscordColor.HotPink,
+        };
+        await commandContext.Channel.SendMessageAsync(embed: thirdGunEmber);
+        var fourthGunEmber = new DiscordEmbedBuilder
+        {
+            Title = "Fourth Gun",
+            Description = "Cost",
+            Color = DiscordColor.Yellow,
+        };
+        await commandContext.Channel.SendMessageAsync(embed: fourthGunEmber);
+        var fifthGunEmber = new DiscordEmbedBuilder
+        {
+            Title = "Fifth Gun",
+            Description = "Cost",
+            Color = DiscordColor.Orange,
+        };
+        await commandContext.Channel.SendMessageAsync(embed: fifthGunEmber);
     }
 }
